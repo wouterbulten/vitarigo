3752478dbf5407af0f07255a5af2af888fa86d85
Wouter Bulten <wouterbulten@mai-ai.org>
2015-07-12 15:39:54 +0200

    Init

---
+++ tasks/transpile.js
@@ -0,0 +1,45 @@
+var browserify = require('browserify');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var config = require('../config');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+var development = (config.env === 'development');
+
+module.exports = function (gulp, entries, outputDir, includeTests) {
+  return function () {
+
+    if(includeTests) {
+      entries = entries.concat(config.tests);
+    }
+
+    // set up the browserify instance on a task basis
+    var b = browserify({
+      entries: entries,
+      debug: development,
+      transform: [babelify]
+    });
+
+    var pipe = b.bundle()
+    .pipe(source('slacjs-app.js'))
+    .pipe(buffer());
+
+    if(development) {
+      pipe = pipe.pipe(sourcemaps.init({loadMaps: true}))
+      .on('error', gutil.log);
+    }
+
+    if(!development) {
+      pipe = pipe.pipe(uglify());
+    }
+
+    if(development) {
+      pipe = pipe.pipe(sourcemaps.write('.'));
+    }
+
+    return pipe.pipe(gulp.dest(outputDir));
+  };
+};
